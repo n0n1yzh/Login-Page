e4c8e748f070e784f46c92fb2f7801a2e5bf1be7
n0n1yzh <1160562880@qq.com>
2021-04-30 20:14:32 +0800

    Initial Commit

---
+++ Login Page/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login Page
+//
+//  Created by YZH on 2021/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login Page/Login_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_PageApp.swift
+//  Login Page
+//
+//  Created by YZH on 2021/4/30.
+//
+
+import SwiftUI
+
+@main
+struct Login_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
